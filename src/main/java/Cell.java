01fec85b177ed9087da275a5c570eca162af6f10
dkoltai <david.koltai@smail.th-koeln.de>
2021-06-01 20:58:33 +0200

    Added full functions for Cell.java

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,12 +1,54 @@
-public class Cell {
-    public boolean getState() {
-        return true;
+import java.util.Random;
+
+enum LiveState {
+    ALIVE,
+    DEAD
+}
+
+class Cell {
+    private LiveState liveState;
+
+    public Cell() {
+        this.liveState = LiveState.DEAD;
     }
 
-    public void changeState() {
+    public LiveState getLiveState() {
+        return this.liveState;
     }
 
-    public boolean getNextStageByNumberOfLiveNeighbors(int liveNeighbours) {
-        return false;
+    public void kill() {
+        this.liveState = LiveState.DEAD;
+    }
+
+    public void resurrect() {
+        this.liveState = LiveState.ALIVE;
+    }
+
+    public LiveState getNextStageByNumberOfLiveNeighbors(int liveNeighbours) {
+        if (liveCellDiesOfOverpopulation(liveNeighbours) || liveCellDiesOfLoneliness(liveNeighbours)) {
+            return LiveState.DEAD;
+        }
+        else if (liveCellLivesOn(liveNeighbours) || deadCellGetsReborn(liveNeighbours)) {
+            return LiveState.ALIVE;
+        }
+        else {
+            return LiveState.DEAD;
+        }
+    }
+
+    private boolean liveCellDiesOfLoneliness(int liveNeighbours) {
+        return this.liveState == LiveState.ALIVE && liveNeighbours < 2;
+    }
+
+    private boolean liveCellDiesOfOverpopulation(int liveNeighbours) {
+        return this.liveState == LiveState.ALIVE && liveNeighbours > 3;
+    }
+
+    private boolean liveCellLivesOn(int liveNeighbours) {
+        return this.liveState == LiveState.ALIVE && (liveNeighbours == 2 || liveNeighbours == 3);
+    }
+
+    private boolean deadCellGetsReborn(int liveNeighbours) {
+        return this.liveState == LiveState.DEAD && liveNeighbours == 3;
     }
 }
--- src/test/java/CellTest.java
+++ src/test/java/CellTest.java
@@ -8,64 +8,64 @@ public class CellTest {
     @Test
     public void InitialStateIsDead(){
         Cell cell = new Cell();
-        assertEquals(false, cell.getState());
+        assertEquals(LiveState.DEAD, cell.getLiveState());
     }
 
     @Test
     public void DeadCellCanGainLife(){
         Cell cell = new Cell();
-        cell.changeState();
-        assertEquals(true, cell.getState());
+        cell.resurrect();
+        assertEquals(LiveState.ALIVE, cell.getLiveState());
     }
 
     @Test
     public void LiveCellCanDie(){
         Cell cell = new Cell();
-        cell.changeState();
-        cell.changeState();
-        assertEquals(false, cell.getState());
+        cell.resurrect();
+        cell.kill();
+        assertEquals(LiveState.DEAD, cell.getLiveState());
     }
 
     @Test
     public void LiveCellDiesOfLoneliness(){
         Cell cell0 = new Cell();
-        cell0.changeState();
+        cell0.resurrect();
         Cell cell1 = new Cell();
-        cell0.changeState();
+        cell0.resurrect();
 
-        assertEquals(false, cell0.getNextStageByNumberOfLiveNeighbors(0));
-        assertEquals(false, cell1.getNextStageByNumberOfLiveNeighbors(1));
+        assertEquals(LiveState.DEAD, cell0.getNextStageByNumberOfLiveNeighbors(0));
+        assertEquals(LiveState.DEAD, cell1.getNextStageByNumberOfLiveNeighbors(1));
     }
 
     @Test
     public void LiveCellLivesOn() { //ToDieAnotherCycle
         Cell cell2 = new Cell();
-        cell2.changeState();
+        cell2.resurrect();
         Cell cell3 = new Cell();
-        cell3.changeState();
+        cell3.resurrect();
 
-        assertEquals(true, cell2.getNextStageByNumberOfLiveNeighbors(2));
-        assertEquals(true, cell3.getNextStageByNumberOfLiveNeighbors(3));
+        assertEquals(LiveState.ALIVE, cell2.getNextStageByNumberOfLiveNeighbors(2));
+        assertEquals(LiveState.ALIVE, cell3.getNextStageByNumberOfLiveNeighbors(3));
     }
 
     @Test
     public void LiveCellDiesOfBeingSquishedByOverpopulation(){
         Cell cell4 = new Cell();
-        cell4.changeState();
-        assertEquals(false, cell4.getNextStageByNumberOfLiveNeighbors(4));
+        cell4.resurrect();
+        assertEquals(LiveState.DEAD, cell4.getNextStageByNumberOfLiveNeighbors(4));
     }
 
     @Test
     public void DeadCellGetsRebornThroughNeighboringReproduction(){
         Cell cell = new Cell();
-        assertEquals(true, cell.getNextStageByNumberOfLiveNeighbors(3));
+        assertEquals(LiveState.ALIVE, cell.getNextStageByNumberOfLiveNeighbors(3));
     }
 
     @Test
     public void DeadCellDoesntGetRebornBecauseOfAWrongNumberOfNeigbours(){
         Cell cell2 = new Cell();
-        assertEquals(false, cell2.getNextStageByNumberOfLiveNeighbors(2));
+        assertEquals(LiveState.DEAD, cell2.getNextStageByNumberOfLiveNeighbors(2));
         Cell cell4 = new Cell();
-        assertEquals(false, cell4.getNextStageByNumberOfLiveNeighbors(4));
+        assertEquals(LiveState.DEAD, cell4.getNextStageByNumberOfLiveNeighbors(4));
     }
 }
